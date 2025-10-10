dcb2372d2d1f91f7b155d319e1375670b7bf131e
khankaif4877-ux <khankaif4877@gmail.com>
2025-10-10 17:33:10 +0530

    newly added page

--- Book_Shop_System/src/main/java/com/client/BookShopSystem/ObjectRepository/LoginPage.java
+++ Book_Shop_System/src/main/java/com/client/BookShopSystem/ObjectRepository/LoginPage.java
@@ -1,15 +1,24 @@
 package com.client.BookShopSystem.ObjectRepository;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 	WebDriver driver;
-
+	WelcomePage wP ;
 	public LoginPage(WebDriver driver) {
 	this.driver = driver;
+	wP=new WelcomePage(driver);
 	PageFactory.initElements(driver, this);
 
+	}
+	
+	@FindBy(id = "CustomerEmail")
+	private WebElement emailTF ;
+	public WebElement getEmailTF() {
+		return emailTF;
 	}
 
 }
--- Book_Shop_System/src/main/java/com/client/BookShopSystem/ObjectRepository/WelcomePage.java
+++ Book_Shop_System/src/main/java/com/client/BookShopSystem/ObjectRepository/WelcomePage.java
@@ -1,6 +1,8 @@
 package com.client.BookShopSystem.ObjectRepository;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class WelcomePage {
@@ -11,4 +13,11 @@ public class WelcomePage {
 	PageFactory.initElements(driver, this);
 
 	}
+	
+	@FindBy(xpath = "//span[.='Account']")
+	private WebElement accountIcon ;
+	public WebElement getAccountIcon() {
+		return accountIcon;
+	}
+	
 }
