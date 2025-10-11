947eedd4c2fc68f19b8260be02c1c56ebe269d29
DELL <DELL@DESKTOP-9060QJV>
2025-10-10 23:06:35 -0700

    update again1

--- Book_Shop_System/src/test/java/com/client/BookShop/Module/Demo.java
+++ Book_Shop_System/src/test/java/com/client/BookShop/Module/Demo.java
@@ -3,7 +3,8 @@ package com.client.BookShop.Module;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("hello");	
+		System.out.println("hello");
+		System.out.println("hello again");
 		
 	}
 
